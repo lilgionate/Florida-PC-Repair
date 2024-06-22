9ce6924a5f4d9332c3e007f7b315dd1c40a25584
lilgionate <140750280+lilgionate@users.noreply.github.com>
2024-06-22 15:04:26 -0400

    creating shipping request

--- Portfolio/src/components/DataRecovery/DataRecoveryShippingRequest/DataRecoveryShippingRequest.jsx
+++ Portfolio/src/components/DataRecovery/DataRecoveryShippingRequest/DataRecoveryShippingRequest.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './DataRecoveryShippingRequest.scss';
 
 export default function DataRecoveryShippingRequest() {
   return (
-    <div>DataRecoveryShippingRequest</div>
-  )
+    <div className="data-shipping-request-container">
+      <div className="data-shipping-request-flex-container">
+        <div className="data-shipping-request-text"><span className='data-shipping-request-steps'>1</span> Device Type</div>
+        <div className="data-shipping-request-text"><span className='data-shipping-request-steps'>2</span> Data Loss Reason</div>
+        <div className="data-shipping-request-text"><span className='data-shipping-request-steps'>3</span> Your details</div>
+      </div>
+      <div className='data-dotted-border'></div>
+      <p className="data-shipping-request-des">
+        Please complete the form so we'll be on the look out for your device. Then, print out the email confirmation you'll receive. Include it with your shipment. Then, you'll receive a free diagnostic including cost and timing for your approval.
+      </p>
+      <p className="data-shipping-request-des-select">What type of device do you need data recovery for?</p>
+      <select className="data-shipping-request-select">
+        <option>Laptop</option>
+        <option>Desktop</option>
+        <option>External Hard Drive</option>
+        <option>Other</option>
+      </select>
+      <div className='data-dotted-border'></div>
+      <button className="data-shipping-request-button">Next</button>
+    </div>
+  );
 }
--- Portfolio/src/routes/DataRecoveryShipping.jsx
+++ Portfolio/src/routes/DataRecoveryShipping.jsx
@@ -1,9 +1,17 @@
-
 import DataRecoveryShippingRequest from '../components/DataRecovery/DataRecoveryShippingRequest/DataRecoveryShippingRequest';
+import HeroTop from '../components/Header/Hero-Top';
+import HomeImg from '../assets/data-header.avif'; 
+
+
 
 function DataRecovery () {
   return (
     <>
+    <HeroTop
+      className="DataShippingHero"
+      heroImg={HomeImg}
+      title={<span className="data-shipping-title">Data Recovery Mail-in Request</span>}
+      />
       <DataRecoveryShippingRequest />
     </>
   )
