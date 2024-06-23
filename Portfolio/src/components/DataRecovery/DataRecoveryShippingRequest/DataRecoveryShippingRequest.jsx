5c65fce5ec3c06887ef34707abd266646244aa97
lilgionate <140750280+lilgionate@users.noreply.github.com>
2024-06-23 18:20:59 -0400

    almost completed shipping request form

--- Portfolio/src/components/DataRecovery/DataRecoveryShippingRequest/DataRecoveryShippingRequest.jsx
+++ Portfolio/src/components/DataRecovery/DataRecoveryShippingRequest/DataRecoveryShippingRequest.jsx
@@ -108,17 +108,121 @@ export default function DataRecoveryShippingRequest() {
                 <input type="checkbox" name="dataLossReason" value="Other" onChange={handleChange}/> Other
               </label>
             </div>
+
+            <p className="data-shipping-request-des-select">Please jot down your priority files and folders. Or note if all must be recovered.</p>
+            <textarea type="text" name="dataLossReason" className="data-shipping-request-textarea" />
+            <p className="data-shipping-request-des-select-two">Please enter any access details such Password / Passcode / Encryption key / PIN.</p>
+            <textarea type="text" name="dataLossReason" className="data-shipping-request-textarea" />
+            <p className="data-shipping-request-des-info">This will help us expedite the process. Do note that we take security very seriously and that form is completely secure.</p>
             <div className='data-dotted-border'></div>
             <div className="data-shipping-request-button-group">
               <button type="button" className="data-shipping-request-button" onClick={handlePrevious}>Previous</button>
               <button type="button" className="data-shipping-request-button" onClick={handleNext}>Next</button>
             </div>
+            
           </>
         )}
         {step === 3 && (
           <>
-            <p className="data-shipping-request-des">Enter your details</p>
-            <input type="text" name="userDetails" className="data-shipping-request-select" placeholder="Your Details" onChange={handleChange} />
+
+            <div className="data-shipping-request-info">
+              <label className="data-shipping-request-label" htmlFor="name">Name</label>
+              <div className="data-shipping-request-name">
+                <input className="data-shipping-request-info-text" type="text" id="first-name" placeholder="First" />
+                <input className="data-shipping-request-info-text" type="text" id="last-name" placeholder="Last" />
+              </div>
+            </div>
+            <div className="data-shipping-request-info">
+              <label className="data-shipping-request-label" htmlFor="email">Email</label>
+              <input className="data-shipping-request-info-text" type="email" id="email" />
+            </div>
+            <div className="data-shipping-request-info">
+              <label className="data-shipping-request-label" htmlFor="phone">Phone</label>
+              <input className="data-shipping-request-info-text" type="tel" id="phone" />
+            </div>
+            <div className="data-shipping-request-info">
+              <h5 className="data-shipping-request-label-title">Shipping</h5>
+              <p className="data-shipping-request-text-description">Please enter your details so we can get in touch with you. Do note that for every completed repair that you approve, we will cover the shipping cost and also arrange for a free return shipping.</p>
+            </div>
+            <div className="data-shipping-request-info">
+              <label className="data-shipping-request-label" htmlFor="address">Your Address</label>
+              <input type="text" className="data-shipping-request-info-text" placeholder="Street Address" />
+            </div>
+            <div className="data-shipping-request-info">
+              <input type="text" className="data-shipping-request-info-text" placeholder="Address Line 2" />
+            </div>
+            <div className="data-shipping-request-name">
+              <input
+                type="text"
+                className="data-shipping-request-info-text"
+                placeholder="City"
+              />
+              <select className="data-shipping-request-info-text" >
+              <option value="" disabled selected hidden>State</option>
+                <option>Alabama</option>
+                <option>Alaska</option>
+                <option>American Samoa</option>
+                <option>Arizona</option>
+                <option>Arkansas</option>
+                <option>California</option>
+                <option>Colorado</option>
+                <option>Connecticut</option>
+                <option>Delaware</option>
+                <option>District of Columbia</option>
+                <option>Florida</option>
+                <option>Georgia</option>
+                <option>Guam</option>
+                <option>Hawaii</option>
+                <option>Idaho</option>
+                <option>Illinois</option>
+                <option>Indiana</option>
+                <option>Iowa</option>
+                <option>Kansas</option>
+                <option>Kentucky</option>
+                <option>Louisiana</option>
+                <option>Maine</option>
+                <option>Maryland</option>
+                <option>Massachusetts</option>
+                <option>Michigan</option>
+                <option>Minnesota</option>
+                <option>Mississippi</option>
+                <option>Missouri</option>
+                <option>Montana</option>
+                <option>Nebraska</option>
+                <option>Nevada</option>
+                <option>New Hampshire</option>
+                <option>New Jersey</option>
+                <option>New Mexico</option>
+                <option>New York</option>
+                <option>North Carolina</option>
+                <option>North Dakota</option>
+                <option>Northern Mariana Islands</option>
+                <option>Ohio</option>
+                <option>Oklahoma</option>
+                <option>Oregon</option>
+                <option>Pennsylvania</option>
+                <option>Puerto Rico</option>
+                <option>Rhode Island</option>
+                <option>South Carolina</option>
+                <option>South Dakota</option>
+                <option>Tennessee</option>
+                <option>Texas</option>
+                <option>U.S. Virgin Islands</option>
+                <option>Utah</option>
+                <option>Vermont</option>
+                <option>Virginia</option>
+                <option>Washington</option>
+                <option>West Virginia</option>
+                <option>Wisconsin</option>
+                <option>Wyoming</option>
+                <option>Armed Forces Americas</option>
+                <option>Armed Forces Europe</option>
+                <option>Armed Forces Pacific</option>
+              </select>
+            </div>
+            <div>
+              <input type="text" className="data-shipping-request-info-text-half" placeholder="ZIP Code" />
+            </div>
             <div className='data-dotted-border'></div>
             <div className="data-shipping-request-button-group">
               <button type="button" className="data-shipping-request-button" onClick={handlePrevious}>Previous</button>
