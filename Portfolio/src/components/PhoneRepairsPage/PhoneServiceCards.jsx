06f58321ce1ee09038d96cbafd49368636024719
lilgionate <140750280+lilgionate@users.noreply.github.com>
2024-06-24 21:06:33 -0400

    added some styles

--- Portfolio/src/components/PhoneRepairsPage/PhoneServiceCards.jsx
+++ Portfolio/src/components/PhoneRepairsPage/PhoneServiceCards.jsx
@@ -6,14 +6,20 @@ import Ipad from '../../assets/ipad.avif';
 import SurfacePro from '../../assets/surface.avif';
 import Others from '../../assets/others.webp';
 
-const RepairCard = ({ title, description, imageUrl }) => {
+const RepairCard = ({ title, description, imageUrl, isSpecial }) => {
   return (
     <div className="phone-card">
-      <img src={imageUrl} alt={title} className="phone-card-image" />
+      <div className="phone-card-image-container">
+        <img src={imageUrl} alt={title} className="phone-card-image" />
+      </div>
       <div className="phone-card-content">
         <h3 className="phone-card-title">{title}</h3>
         <p className="phone-card-text">{description}</p>
-        <button className="phone-card-button">Learn More</button>
+        {isSpecial ? (
+          <button className="phone-card-button">Schedule a Free Diagnostic</button>
+        ) : (
+          <button className="phone-card-button">Learn More</button>
+        )}
       </div>
     </div>
   );
@@ -21,10 +27,8 @@ const RepairCard = ({ title, description, imageUrl }) => {
 
 const RepairCardsContainer = () => {
   return (
-    <>
-    
     <div className="phone-card-container">
-    <h2 className='phone-header'>Servicing Apple devices, Microsoft tablets, and more</h2>
+      <h2 className="phone-header">Servicing Apple devices, Microsoft tablets, and more</h2>
       <RepairCard
         title="iPhone Repair"
         description="Trustworthy Out-of-Warranty iPhone Repair"
@@ -44,9 +48,9 @@ const RepairCardsContainer = () => {
         title="Other Devices"
         description="If you have any other devices not listed here, simply bring it to our repair center for a free diagnostic"
         imageUrl={Others}
+        isSpecial
       />
     </div>
-    </>
   );
 };
 
