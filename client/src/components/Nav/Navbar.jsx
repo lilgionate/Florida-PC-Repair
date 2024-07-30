a7086e0adae0ebb20c3e50f1307d3c7ddc89ad8c
lilgionate <140750280+lilgionate@users.noreply.github.com>
2024-07-29 21:36:26 -0400

    heroku

--- client/src/components/Nav/Navbar.jsx
+++ client/src/components/Nav/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import "./navbar.scss";
@@ -59,7 +59,7 @@ function Navbar() {
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="http://localhost:5173/" className="navbar-logo">
+          <Link to="http://localhost:3000/" className="navbar-logo">
             <img src={logo} alt="" />
           </Link>
           <div className="menu-icons" onClick={handleClick}>
