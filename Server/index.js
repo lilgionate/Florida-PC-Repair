3a74b192ba5d3d79820cef0d57ea98b36f05da33
lilgionate <140750280+lilgionate@users.noreply.github.com>
2024-07-30 21:09:30 -0400

    posting live

--- Server/index.js
+++ Server/index.js
@@ -1,19 +1,20 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../client')));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(cors())
+app.use(cors());
 
 /*Custom PC Sub */
-
 app.post('/sub', async (req, res) => {
     const {name, payment_method} = req.body;
   
@@ -391,7 +392,7 @@ app.post('/sub11', async (req, res) => {
 
 app.get("/", function (req, res) {
     res.sendFile(
-        path.join(__dirname, "../client/public/index.html"),
+        path.join(__dirname, "../client/index.html"),
         function (err) {
             if (err) {
                 res.status(500).send(err);
@@ -400,6 +401,6 @@ app.get("/", function (req, res) {
     );
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.js'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-)
+);
